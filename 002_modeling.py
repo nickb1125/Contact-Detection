d8be53e2cd5d69c676438120e6d38b96b1af042a
nrb33 <nrb33@duke.edu>
2024-05-09 15:07:09 -0400

    minor fix

--- 002_modeling.py
+++ 002_modeling.py
@@ -92,7 +92,7 @@ for epoch in range(num_epochs):
     combined_model.train()
     total_loss = 0
     total_samples = len(dataset)
-    for batch_idx, (features, labels) in enumerate(dataset):
+    for batch_idx, (features, labels) in enumerate(dataloader):
         x1, x2, x3 = features
         x1, x2, x3 = x1.to(device), x2.to(device), x3.to(device)
 
